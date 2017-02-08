b0553a34b8e7c2dc91afa4971448c85ba40ce103
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-08 15:13:32 +0100

    - removed deprecated files

--- app/components/camera.jsx
+++
@@ -1,8 +0,0 @@
-import {Entity} from 'aframe-react';
-import React from 'react';
-
-export default props => (
-	<Entity>
-		<Entity id="camera" camera look-controls user-height="0" {...props}/>
-	</Entity>
-);
