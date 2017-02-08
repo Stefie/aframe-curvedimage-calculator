a2823727f3fccff7eb44d088564ef100112ee962
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-08 15:16:20 +0100

    + aframe calculator preview

--- app/components/calculatorPreview.jsx
+++ app/components/calculatorPreview.jsx
@@ -1,13 +1,49 @@
 import 'aframe';
 import {Entity, Scene} from 'aframe-react';
-import {CurvedImage} from 'aframe-react-components';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CalculatorPreview extends React.Component {
-	render() {
-		return (
-			<Entity primitive='a-curvedimage' material={{ color: '#ffffff', src: 'http://3.bp.blogspot.com/-XJeDRKJkryg/VZ6HYZfeAEI/AAAAAAAACmQ/rpLlAR3LPLw/s1600/PeakyIslands-3.jpg ', transparent:'false' }} geometry={{ thetaLength: '60', thetaStart: '150', radius: '5', height: '2.62' }} />
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let loading = ( this.props.appState == 'loading' ) ? 'true' : 'false' ;
+    return (
+      <Scene>
+        <a-assets>
+          <img src="./app/assets/images/tutorial.png" id="tutorial" />
+          <img src="./app/assets/images/error.png" id="error" />
+          <img src="./app/assets/images/error-material.png" id="error-material" />
+          <img src="./app/assets/images/loading.png" id="loading" />
+          <img src="./app/assets/images/free.png" id="free" />
+        </a-assets>
+        <Entity id="preview-wrapper" position="0 1 0">
+          <Entity id="curvedimage-preview"
+
+            material={{
+              side: 'double',
+              transparent:'true',
+              shader: 'flat',
+              repeat: '-1 1',
+              src: this.props.assetSrc
+            }}
+            geometry={{
+              primitive: 'cylinder',
+              openEnded: true,
+              segmentsRadial: 96,
+              thetaLength: this.props.thetaLength,
+              thetaStart: this.props.thetaStart,
+              radius: this.props.r,
+              height: this.props.h }} />
+        </Entity>
+        <Entity id="sky" primitive='a-sky' material={{ color: '#f0f0f0' }} />
+
+        <a-text value='loading...' color='#21897C' align='center' position='0 1 -3' scale='3 3 1'  visible={loading} >
+          <a-animation attribute="scale" easing="linear" dur="800" direction="alternate" repeat="indefinite" from="3 3  1" to="3.1 3.1 1"></a-animation>
+        </a-text>
+
+      </Scene>
+    );
+  }
 }
