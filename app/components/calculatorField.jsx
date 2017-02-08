a1d61bb867e6ec36dac03b4ca4e7ffe548a8f0d0
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-08 15:44:04 +0100

    ~ spaces to tabs

--- app/components/calculatorField.jsx
+++ app/components/calculatorField.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CalculatorField extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  _handleFocus(e) {
-    e.target.select();
-  }
-  render() {
-    let opts = {};
-    opts['min'] = !this.props.type ? '0.0000' : '';
+	constructor(props) {
+		super(props);
+	}
+	_handleFocus(e) {
+		e.target.select();
+	}
+	render() {
+		let opts = {};
+		opts['min'] = !this.props.type ? '0.0000' : '';
 
-    return (
-      <div className="flex-wrapper">
-        <label>{ this.props.label }</label>
-        <div className="field-wrapper">
-          <input type={ this.props.type ? this.props.type : 'number' }
-                 onFocus={this._handleFocus.bind(this)}
-                 onChange={this.props.onChange }
-                 value={ this.props.value }
-                 placeholder={ this.props.placeholder }
-                 max={ this.props.max }
-                 disabled={ this.props.disabled }
-                 {...opts}  />
-          <span>{ this.props.postfix }</span>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className="flex-wrapper">
+				<label>{ this.props.label }</label>
+				<div className="field-wrapper">
+					<input
+						type={ this.props.type ? this.props.type : 'number' }
+						onFocus={this._handleFocus.bind(this)}
+						onChange={this.props.onChange }
+						value={ this.props.value }
+						placeholder={ this.props.placeholder }
+						max={ this.props.max }
+						disabled={ this.props.disabled }
+						{...opts}  />
+					<span>{ this.props.postfix }</span>
+				</div>
+			</div>
+		);
+	}
 }
