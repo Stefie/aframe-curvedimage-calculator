d85627bd4d548521a7a8d48a615e429e5f57dfa7
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-10 13:12:42 +0100

    ~ footer wording

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,7 +9,7 @@ export default class Footer extends React.Component {
 		let loading = ( this.props.appState == 'loading' ) ? 'true' : 'false' ;
 		return (
 			<footer className="page-footer overlay-content ">
-				<p>Curvedimage Calculator - A Helper App for <a href="https://aframe.io/docs/0.4.0/primitives/a-curvedimage.html" target="blank" title="A-Frame Docs:  a-curvedimage">A-Frame's a-curvedimage component</a></p>
+				<h2 className="footer-content">Curvedimage Calculator - A Helper App for <a href="https://aframe.io/docs/0.5.0/primitives/a-curvedimage.html" target="blank" title="A-Frame Docs:  a-curvedimage">A-Frame's &lt;a-curvedimage&gt; component</a></h2>
 			</footer>
 		);
 	}
