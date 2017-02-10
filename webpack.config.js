8229b82dac1fe0f1ff00a19cf1b52ace631d207c
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-10 15:10:22 +0100

    ~ bundle dump

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 	resolve: {
 		// Tell webpack to look for required files in node
 		modulesDirectories: ['node_modules'],
-		extensions: ['', '.js', '.jsx', '.json']
+		extensions: ['', '.js', '.jsx', '.json', '.styl']
 	},
 	module: {
 		// npParse === aframe Bugfix for console warning
