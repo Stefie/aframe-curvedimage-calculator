defe8d08c82720aeb5c4d0de14995b3adde55793
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-10 13:09:40 +0100

    ~ webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,52 @@ poststylus = require('poststylus'),
 HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: __dirname + "/app",
-  entry: ['babel-polyfill','./app.jsx'],
-  output: {
-    path: __dirname + '/docs',
-    filename: "bundle.js"
-  },
-  resolve: {
-    // Tell webpack to look for required files in node
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.js', '.jsx', '.json', '.styl']
-  },
-  module: {
-    // npParse === aframe Bugfix for console warning
-    noParse: [
-      /node_modules\/aframe\/dist\/aframe-master.js/, // for aframe from NPM
-      /node_modules\/cannon\/build\/cannon.js/, // for aframe-extras from NPM
-    ],
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'stage-0', 'react'],
-        }
+	context: __dirname + "/app",
+	entry: ['babel-polyfill','./app.jsx'],
+	output: {
+		path: __dirname + '/docs',
+		filename: "bundle.js"
+	},
+	resolve: {
+		// Tell webpack to look for required files in node
+		modulesDirectories: ['node_modules'],
+		extensions: ['', '.js', '.jsx', '.json']
+	},
+	module: {
+		// npParse === aframe Bugfix for console warning
+		noParse: [
+			/node_modules\/aframe\/dist\/aframe-master.js/, // for aframe from NPM
+			/node_modules\/cannon\/build\/cannon.js/, // for aframe-extras from NPM
+		],
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'stage-0', 'react'],
+				}
 
-      },{
-        test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      }
-    ]
-  },
-  stylus: {
-    preferPathResolver: 'webpack',
-    use: [
-      poststylus([ 'autoprefixer'])
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'A-Frame Curverimge Calculator',
-      template: __dirname + '/app/index.html',
-      inject: 'body',
-      files: {
-        'css': [ __dirname + '/app/style.styl']
-      }
-    })
-  ],
-  devServer: {
-    host: '0.0.0.0',
-    port: 8090
-  }
+			},{
+				test: /\.styl$/,
+				loader: 'style-loader!css-loader!stylus-loader'
+			}
+		]
+	},
+	stylus: {
+		preferPathResolver: 'webpack',
+		use: [
+			poststylus([ 'autoprefixer'])
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'A-Frame Curverimge Calculator',
+			template: __dirname + '/app/index.html',
+			inject: 'body',
+			files: {
+				'css': [ __dirname + '/app/style.styl']
+			}
+		})
+	]
 };
