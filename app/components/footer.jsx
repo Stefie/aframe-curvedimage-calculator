5c625fc0dc599bd3e6a03f17a4897d91264deb80
Stefanie Doll <stefie.doll@googlemail.com>
2017-02-13 12:05:13 +0100

    + added link to a-frame docs on 'CORS header' error

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,14 +5,19 @@ export default class Footer extends React.Component {
   constructor(props) {
     super(props);
   }
-  _generateHelpLinks(){
-    let helpLinks = <div className="help-links"><a href="https://aframe.io/docs/0.5.0/introduction/faq.html#why-does-my-asset-e-g-image-video-model-not-load" target="blank" title="Visit A-Frame's FAQ">Read more about CORS Header</a></div>;
+  render() {
+    let helpLinks = null;
+    if(this.props.appState == 'errorMaterial'){
+      console.log('Yo?');
+      helpLinks = <div className="help-links"><a href="https://aframe.io/docs/0.5.0/introduction/faq.html#why-does-my-asset-e-g-image-video-model-not-load" target="blank" title="Visit A-Frame's FAQ">Read more about missing CORS headers</a></div>;
+    }else{
+      console.log('no yo');
     }
-    render() {
-      return (
-        <footer className="page-footer overlay-content ">
-          <h2 className="footer-content">Curvedimage Calculator - A Helper App for <a href="https://aframe.io/docs/0.5.0/primitives/a-curvedimage.html" target="blank" title="A-Frame Docs:  a-curvedimage">A-Frame's &lt;a-curvedimage&gt; component</a></h2>
-        </footer>
-      );
-    }
+    return (
+      <footer className="page-footer overlay-content ">
+        { helpLinks }
+        <h2 className="footer-content">Curvedimage Calculator - A Helper App for <a href="https://aframe.io/docs/0.5.0/primitives/a-curvedimage.html" target="blank" title="A-Frame Docs:  a-curvedimage">A-Frame's &lt;a-curvedimage&gt; component</a></h2>
+      </footer>
+    );
   }
+}
